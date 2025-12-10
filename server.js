ff9606d52b61112fdefae912e61e1e262a13beaf
mikey mike <mikaelworld25@gmail.com>
2025-12-10 12:09:20 +0300

    asdas

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Clicker Game API запущен на порту ${PORT}`);
-  console.log(`Откройте браузер: http://localhost:${PORT}`);
+  console.log(`API запущен на порту ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
